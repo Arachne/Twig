8f21f4b746edf1d68cda8e8408e289fd8fd78326
Jáchym Toušek <enumag@gmail.com>
2016-06-20 23:10:48 +0200

    Refactor tests

--- tests/integration/src/TwigExtensionTest.php
+++ tests/integration/src/TwigExtensionTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Integration;
 
-use Arachne\Bootstrap\Configurator;
 use Codeception\Test\Unit;
 use Tracy\Dumper;
 use Twig_Environment;
@@ -14,10 +13,8 @@ class TwigExtensionTest extends Unit
 {
     public function testConfiguration()
     {
-        $container = $this->createContainer('config.neon');
-
         /* @var $twig Twig_Environment */
-        $twig = $container->getByType(Twig_Environment::class);
+        $twig = $this->tester->grabService(Twig_Environment::class);
         $this->assertInstanceOf(Twig_Environment::class, $twig);
 
         // Fix dump result comparison on linux.
@@ -30,14 +27,5 @@ class TwigExtensionTest extends Unit
         $this->assertSame('"long_val ... " (10)', trim($twig->render('@namespace/index.twig', [
             'bar' => 'long_value',
         ])));
-    }
-
-    private function createContainer($file)
-    {
-        $config = new Configurator();
-        $config->setTempDirectory(TEMP_DIR);
-        $config->addConfig(__DIR__.'/../config/'.$file);
-
-        return $config->createContainer();
     }
 }
